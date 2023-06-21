0d9e895baf718d5e6539e20966f483055e2eabff
Neelesh Singh <neelesh@Neeleshs-MacBook-Air.local>
2023-06-21 18:17:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+/* Client Code 
+
+let sse = new EventSource("http://localhost:8888/stream");
+sse.onmessage = console.log
+
+*/
+
+const cors = require("cors");
+const express = require("express");
+const app = require("express")();
+
+const corsOption = {
+  origin: ["*"],
+  credentials: true,
+};
+app.use(cors(corsOption));
+app.use(express.json());
+
+// Increase the timeout to 10 minutes (600000 milliseconds)
+app.set("timeout", 600000);
+
+app.get("/", (req, res) => res.send("hello!"));
+
+// app.get("/stream", (req, res) => {
+//   console.log("hit");
+//   res.setHeader("Content-Type", "text/event-stream");
+//   send(res);
+// });
+const port = process.env.PORT || 8888;
+const serverName = process.env.SERVER_NAME || "sample";
+
+// let i = 0;
+// function send(res) {
+//   res.write("data: " + `hello from ${serverName} ---- [${i}]\n\n`);
+//   i += 1;
+
+//   setTimeout(() => send(res), 1000);
+// }
+
+app.get("/poll", (req, res) => {
+  // Simulating a long polling request that takes longer than 5 minutes
+  //   setTimeout(() => {
+  res.send("Long polling response");
+  //   }, 1000); // Respond after 10 minutes
+});
+
+app.post("/hook", (req, res) => {
+  console.log(req.body);
+  res.send();
+});
+
+app.listen(port);
+console.log(`Listening on ${port}`);
